a2fdd0503c5170148d8c226b9b5c0ae81373751e
KosMosV2 <jesusm1948@gmail.com>
2024-07-24 18:42:45 -0400

    Added deletion and helper deletion functions

--- bst.c
+++ bst.c
@@ -20,6 +20,41 @@ Node * createNode(int data){
     return newNode;
 }
 
+int isLeafNode(Node * root){
+    if(!root->left && !root->right){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int onlyLeftChild(Node * root){
+    if(root->left != NULL && !root->right){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int onlyRightChild(Node * root){
+    if(!root->left && root->right != NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+Node * maxValue(Node * root){
+    if(root->right == NULL){
+        return root;
+    }
+    else{
+        return maxValue(root->right);
+    }
+}
+
 Node * insertBST(Node * root, Node * newNode){
     printf("Inside insert\n");
     if(!root){
@@ -41,6 +76,46 @@ Node * insertBST(Node * root, Node * newNode){
     return root;
 }
 
+Node * deleteNode(Node * root, int data){
+    if(!root){
+        return NULL;
+    }
+
+    if(root->data == data){
+        if(isLeafNode(root)){
+            free(root);
+            return NULL;
+        }
+        else if(onlyLeftChild(root)){
+            Node * tmp = root->left;
+            free(root);
+            return tmp;
+        }
+        else if(onlyRightChild(root)){
+            Node * tmp = root->right;
+            free(root);
+            return tmp;
+        }
+        else{
+            Node * tmp = maxValue(root->left);
+            int maxPredecessor = tmp->data;
+            root->left = deleteNode(root->left, maxPredecessor);
+            root->data = maxPredecessor;
+            return root;
+        }
+    }
+    else{
+        if(data < root->data){
+            root->left = deleteNode(root->left, data);
+        }
+        else if(data > root->data){
+            root->right = deleteNode(root->right, data);
+        }
+    }
+
+    return root;
+}
+
 void preOrderPrint(Node * root){
     if(!root){
         return;
@@ -58,8 +133,9 @@ int main(){
     while(true){
         printf("BST MENU\n");
         printf("1. Insert\n");
-        printf("2. Print PreOrder\n");
-        printf("3. EXIT\n");
+        printf("2. Delete\n");
+        printf("3. Print PreOrder\n");
+        printf("4. EXIT\n");
         int option;
         scanf("%d", &option);
         switch (option)
@@ -72,13 +148,20 @@ int main(){
             printf("New node created successfully\n");
             root = insertBST(root, newNode);
             break;
-
         case 2:
+            printf("Enter value for Deletion: \n");
+            int delValue;
+            scanf("%d", &delValue);
+            printf("Now Deleting %d....\n", delValue);
+            root = deleteNode(root, delValue);
+            break;
+
+        case 3:
             printf("Root -> ");
             preOrderPrint(root);
             printf("\n");
             break;
-        case 3:
+        case 4:
             printf("Bye Bye\n");
             exit(0);
         default:
